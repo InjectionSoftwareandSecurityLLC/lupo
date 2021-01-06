e7de6334ab6b5cfb378b816c9fc2c119a6a1f5ce
3ndG4me <3ndG4me@protonmail.com>
2021-01-06 13:11:24 -0500

    skeleton code for client added interact and sessions

---
+++ lupo-client/main.go
@@ -0,0 +1,12 @@
+// main - the "main" package of the application. defines the entry point of the application.
+package main
+
+import (
+	"github.com/InjectionSoftwareandSecurityLLC/lupo/lupo-client/cmd"
+	"github.com/desertbit/grumble"
+)
+
+// main - executes the primary grumble application defined in the "cmd" package
+func main() {
+	grumble.Main(cmd.App)
+}
