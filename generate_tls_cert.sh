eb1beaa7911a8364c7b4889ab4a75f7d5aea1aef
Casey Erdmann <14339392+3ndG4me@users.noreply.github.com>
2022-07-13 13:33:19 -0400

    Update generate_tls_cert.sh

--- generate_tls_cert.sh
+++ generate_tls_cert.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "This script is just a helper to quickly generate self-signed TLS certs for Lupo C2"
 echo "This script depends on openssl to be installed, so if it is not please install it now..."
+echo "Specify host with the first parameter and domain with the second i.e. 'generate_tls_cert.sh MYIP MYDOMAIN'"
+echo "To specify a custom cert expiry time input number of days into the third parameter i.e. 'generate_tls_cert.sh MYIP MYDOMAIN DAYS'"
 read -n 1 -s -r -p "press enter to continue..."
-DAYS=$1
+IP=$1
+HOST=$2
+DAYS=$3
 echo "Generating private key..."
 echo ""
 openssl genrsa -out lupo-server.key 2048
@@ -13,18 +17,18 @@ if [ ! -z "$DAYS" ];
 then
 echo "Generating a cert with the following parameters:"
 echo ""
-echo "openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days $DAYS -subj \"/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=localhost\"  -addext \"subjectAltName = DNS:localhost, IP:127.0.0.1\""
+echo "openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days $DAYS -subj \"/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=${HOST}\"  -addext \"subjectAltName = DNS:${HOST}, IP:${IP}\""
 echo ""
-openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days $DAYS -subj "/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=localhost"  -addext "subjectAltName = DNS:localhost, IP:127.0.0.1" 
+openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days $DAYS -subj "/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=${HOST}"  -addext "subjectAltName = DNS:${HOST}, IP:${IP}" 
 else
 echo "Generating a cert with the following parameters:"
 echo ""
-echo "openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days 3650 -subj \"/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=localhost\"  -addext \"subjectAltName = DNS:localhost, IP:127.0.0.1\""
+echo "openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days 3650 -subj \"/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=${HOST}\"  -addext \"subjectAltName = DNS:${HOST}, IP:${IP}\""
 echo ""
 echo "To change the number of days pass in a parameter to the script and re-run it"
 echo "example: generate_tls_cert.sh <days>"
 echo ""
-openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days 3650 -subj "/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=localhost" -addext "subjectAltName = DNS:localhost, IP:127.0.0.1" 
+openssl req -new -x509 -sha256 -key lupo-server.key -out lupo-server.crt -days 3650 -subj "/C=US/ST=Lupo/L=Lupo/O=Lupo/OU=Lupo/CN=${HOST}" -addext "subjectAltName = DNS:${HOST}, IP:${IP}" 
 fi
 echo ""
 echo "Generating PEM file to use with Lupo implants/clients..."
