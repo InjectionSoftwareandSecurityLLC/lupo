25b00949f5ded1247be7763249b9a1ac63be7750
Casey Erdmann <14339392+3ndG4me@users.noreply.github.com>
2026-02-25 19:39:31 -0500

    Rename lupo_implant_tcp_coff.go to lupo_implant_tcp_enc_coff.go

--- sample/lupo_implant_tcp_enc_coff.go
+++ sample/lupo_implant_tcp_enc_coff.go
@@ -1,6 +1,6 @@
 package main
 
-// Build: CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -o lupo_implant_tcp_coff.exe lupo_implant_tcp_coff.go
+// Build: CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -o lupo_implant_tcp_enc_coff.exe lupo_implant_tcp_enc_coff.go
 
 import (
 	"bufio"
